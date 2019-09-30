631ee36cf0a21f7432be3a8f4d7e61727545cb9b
Nidasio Alberto <nidasioalberto@gmail.com>
2019-09-30 10:08:30 +0200

    Sistemata la descrizione

--- riduzione_di_gauss/lib_riduzione_di_gauss.c
+++ riduzione_di_gauss/lib_riduzione_di_gauss.c
@@ -9,9 +9,8 @@
  * Descrizione: Questo programma esegue il metodo della riduzione di Gauss
  * che si sviluppa in questi 3 passaggi:
  *   1: Si ordinano le righe per la posizione dei propri pivot
- *   2: Si modificano le righe successive alla prima in modo tale che i loro
- *      pivot non siano nella stessa posizione di quello della prima riga
- *      (tramite la moltiplicazione)
+ *   2: Si modificano le righe successive in modo tale che la matrice diventi
+ *      a scala (tramite la moltiplicazione)
  *   3: Si ripete il procedimento dal punto 1 considerando la matrice senza la
  *      prima riga
 */
