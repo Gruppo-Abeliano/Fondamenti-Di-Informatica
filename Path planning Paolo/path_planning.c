ee3511ec8e33d47f5f4ab65ae95dc8363cc6a61e
Paolo <pertino.liceograssi@gmail.com>
2019-10-05 16:25:25 +0200

    fine path planning - PARTE 2
    manca : commenti alle istruzioni - abbellimento ciclo while

--- Path planning Paolo/path_planning.c
+++ Path planning Paolo/path_planning.c
@@ -8,6 +8,7 @@
 
 //Librerie Utilizzate
 #include <stdio.h>
+#include <stdlib.h>
 
 //Definizione Costanti
 #define MAX_SIZE 10                                   //Dimensione della matrice
@@ -15,15 +16,18 @@
 #define OSTACOLO 1000                                 //Cella contenente un ostacolo
 
 //Definizione tipi
-typedef int matrice[MAX_SIZE][MAX_SIZE];
+typedef int matriceInt[MAX_SIZE][MAX_SIZE];
 
 //Definizione funzioni utilizzate
-void leggiMatrice(matrice mat);
-void pathLenght(matrice mat);
+void leggiMatrice(matriceInt mat,int type);
+void pathLenght(matriceInt mat);
+void cercaMeta(matriceInt mat, int* coordMetaX, int* coordMetaY);
+void pathSearch(matriceInt mat);
+void clearScreen();
 
 //Main
 int main(int argc, char const *argv[]) {
-  matrice mat = {
+  matriceInt mat = {
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
     {-1,-1,-1,-1,0,-1,-1,-1,-1,-1},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
@@ -35,16 +39,29 @@ int main(int argc, char const *argv[]) {
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
     {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}
   };
+  char scelta;
 
   pathLenght(mat);
-  leggiMatrice(mat);
+  leggiMatrice(mat,0);
+
+  printf("\nVuoi visualizzare il tragitto da percorrere da un determinato punto di partenza?\n\na. Si\nb. No\n\nScelta : ");
+  scanf("%c",&scelta);
+
+  if (scelta=='a')
+  {
+    pathSearch(mat);
+    leggiMatrice(mat,1);
+  } else {
+    printf("\nInterruzione programma\n");
+  }
+
   return 0;
 }
 
 //Funzioni
 
 /*** leggiMatrice : stampa a schermo la matrice passata come argomento ***/
-void leggiMatrice(matrice mat)
+void leggiMatrice(matriceInt mat,int type)
 {
   int i,j;
 
@@ -54,7 +71,11 @@ void leggiMatrice(matrice mat)
     j=0;
     while (j<MAX_SIZE)
     {
-      printf("%d\t",mat[i][j]);
+      switch(type)
+      {
+        case 0: printf("%d\t",mat[i][j]); break;
+        case 1: printf("%c\t",(char) mat[i][j]); break;
+      }
       j++;
     }
     printf("\n");
@@ -63,20 +84,24 @@ void leggiMatrice(matrice mat)
 }
 
 /*** pathLenght : esegue l'algoritmo risolutore dell'esercizio per assegnare alle celle il valore della loro distanza dal punto meta 0 ***/
-void pathLenght(matrice mat)
+void pathLenght(matriceInt mat)
 {
   int i,j;                                                      //Definisco i contatori utilizzati
   int currentMaxDistance;
+  int empty_cells;
 
+  empty_cells=1;
   currentMaxDistance=0;
-  while(currentMaxDistance<(MAX_SIZE*2))
+  while(empty_cells != 0)
   {
+    empty_cells=0;
     i=0;
     while(i<MAX_SIZE)
     {
       j=0;
       while(j<MAX_SIZE)
       {
+        if (mat[i][j]==EMPTY_CELL) {empty_cells++;}
         /*
           La matrice viene ciclata MAX_SIZE*2 volte e ad ogni ciclo si occupa dell'aggiornamento delle celle
           adiacenti a quelle che hanno valore pari a currentMaxDistance, solo se esse sono vuote ed assumono
@@ -95,5 +120,88 @@ void pathLenght(matrice mat)
     }
     currentMaxDistance++;
   }
+}
 
+void cercaMeta(matriceInt mat, int* coordMetaX, int* coordMetaY)
+{
+  int i,j;
+
+  i=0;
+  while(i<MAX_SIZE)
+  {
+    j=0;
+    while(j<MAX_SIZE)
+    {
+      if(mat[i][j] == 0)
+      {
+        *coordMetaX = i;
+        *coordMetaY = j;
+      }
+      j++;
+    }
+    i++;
+  }
+}
+
+void pathSearch(matriceInt mat)
+{
+  int minimo;
+  int rigaCercata,colonnaCercata,x,y;
+  int metaX,metaY;
+  int i,j;
+
+  clearScreen();
+
+  printf("\nScegli la posizione da cui partire indicandone le coordinate\n\n Inserisci la riga : ");
+  scanf("%d",&rigaCercata);
+  printf("\nInserisci la colonna : ");
+  scanf("%d",&colonnaCercata);
+
+  x = rigaCercata - 1;
+  y = colonnaCercata - 1;
+
+  if(mat[x][y] != OSTACOLO && mat[x][y] != 0) {
+    mat[x][y] = 'P';
+
+    cercaMeta(mat,&metaX,&metaY);
+
+    while(mat[metaX][metaY] == 0)
+    {
+      if(mat[x][y]==0)
+      {
+        mat[x][y]='G';
+      } else {
+        if (mat[x-1][y]<=mat[x+1][y] && mat[x-1][y]<=mat[x][y-1] && mat[x-1][y]<=mat[x][y+1]) {
+          mat[x][y]='^';
+          x=x-1;
+        } else if(mat[x+1][y]<=mat[x-1][y] && mat[x+1][y]<=mat[x][y-1] && mat[x+1][y]<=mat[x][y+1]) {
+          mat[x][y]='v';
+          x=x+1;
+        } else if(mat[x][y-1]<=mat[x][y+1] && mat[x][y-1]<=mat[x+1][y] && mat[x][y-1]<=mat[x-1][y]) {
+          mat[x][y]='<';
+          y=y-1;
+        } else if(mat[x][y+1]<=mat[x][y-1] && mat[x][y+1]<=mat[x+1][y] && mat[x][y+1]<=mat[x-1][y]) {
+          mat[x][y]='>';
+          y=y+1;
+        }
+      }
+    }
+    i=0;
+    while(i<MAX_SIZE)
+    {
+      j=0;
+      while(j<MAX_SIZE)
+      {
+        if(mat[i][j]==OSTACOLO) { mat[i][j]='*'; }
+        if(mat[i][j]!='<' && mat[i][j]!='>' && mat[i][j]!='v' && mat[i][j]!='^' && mat[i][j]!='P' && mat[i][j]!='G' && mat[i][j]!='*') { mat[i][j]='-'; }
+        j++;
+      }
+      i++;
+    }
+  }
+}
+
+void clearScreen()
+{
+  system("cls");
 }
