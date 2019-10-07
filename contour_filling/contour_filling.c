278f1b96806f6619a00a2f2f659b0edbdf2ff970
Nidasio Alberto <nidasioalberto@gmail.com>
2019-10-07 12:31:46 +0200

    Integrato l'algoritmo di Paolo

--- contour_filling/contour_filling.c
+++ contour_filling/contour_filling.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
+/**
+ * Autori: Paolo Pertino e Alberto Nidasio
+ * 
+ * Scopo: riempire un contorno
+ */
+
 #define L 12 // Larghezza della matrice
 #define A 10 // Altezza della matrice
+#define BORDO_INTERNO 1
+#define BORDO_ESTERNO -1
 
-void mostraPiano(int piano[A][L]);
+// Definisco il tipo di matrice che utilizzderò
+typedef int matrice[A][L];
+typedef enum
+{
+    false, // Valore 0
+    true
+} bool;
+
+// Questa funzione permette di visualizzare il piano nella linea di comando
+void mostraPiano(matrice piano);
+
+// Permette di riempire i contorni presenti in una martrice, (vedi il pdf per definizione di controrno)
+void contourFilling(matrice piano);
 
 int main()
 {
-    int piano[A][L] = {
+    matrice piano = {
         {0, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 0},
         {0, 0, -1, -1, 1, 1, 1, 1, -1, 0, 0, 0},
         {0, -1, 1, 1, 0, 0, 0, 0, 1, -1, 0, 0},
@@ -18,61 +38,13 @@ int main()
         {-1, 1, 1, 0, 1, 1, 1, 1, 0, 1, -1, 0},
         {0, -1, -1, 1, -1, -1, -1, -1, 1, 1, -1, 0},
         {0, 0, 0, -1, 0, 0, 0, 0, -1, -1, 0, 0}};
-    int i, j; // Contatori
-    int t;    // Indica se ho trovato un elemento
 
     printf("Input:\n");
     mostraPiano(piano);
 
-    // Coloro la regione
-    for (i = 0; i < A; i++) // Per ogni riga
-    {
-        // Continuo finchè non ho controllato tutte le colonne
-        j = 0;
-        t = 0;
+    contourFilling(piano);
 
-        for (j = 0; j < L; j++)
-        {
-            // Se non abbiamo già trovato un perimetro di apertura (-1, 1)
-            if (!t)
-            {
-                //Controllo se ci troviamo difronte a un perimetro di apertura
-                if (j < L - 1 && piano[i][j] == -1 && piano[i][j + 1] >= 1)
-                {
-                    t = 1;
-                    j++;
-                }
-            }
-            else
-            {
-                // Se abbiamo già trovato un perimetro in precedenza coloro le celle finchè non ne trovo uno di chiusura (1. -1)
-                if (piano[i][j + 1] != -1) // Non controllo il l'indice che uso viste le ipotesi
-                {
-                    if (piano[i][j] != 1)
-                        piano[i][j] = 2; // Coloro solo se non c'è già 1
-                }
-                else
-                {
-                    t = 0;
-                }
-            }
-        }
-    }
-
-    printf("\nOutput 1:\n");
-    mostraPiano(piano);
-
-    // Sistemo i dati cambiando i -1 con 0 e i 2 con 1
-    for (i = 0; i < A; i++)
-        for (j = 0; j < L; j++)
-        {
-            if (piano[i][j] == -1)
-                piano[i][j] = 0;
-            if (piano[i][j] == 2)
-                piano[i][j] = 1;
-        }
-
-    printf("\nOutput 2:\n");
+    printf("\nOutput :\n");
     mostraPiano(piano);
 
     return (0);
@@ -109,4 +81,45 @@ void mostraPiano(int piano[A][L])
         }
         printf("\n");
     }
+}
+
+void contourFilling(matrice piano)
+{
+    int i, j;     // Contatori
+    bool interno; // Indica se, durante il ciclo, mi trovo nell'area da segnare
+
+    // Coloro la regione
+    for (i = 0; i < A; i++) // Per ogni riga
+    {
+        // Continuo finchè non ho controllato tutte le colonne
+        interno = false;
+
+        for (j = 0; j < L; j++)
+        {
+            // Se non abbiamo già trovato un bordo esterno seguito da uno interno, quindi se siamo all'interno dell'area
+            if (!interno)
+            {
+                // Controllo se ci troviamo difronte a un perimetro di apertura (-1, 1)
+                if (piano[i][j] == BORDO_ESTERNO && j < L - 1 && piano[i][j + 1] == BORDO_INTERNO)
+                {
+                    interno = true;
+                    j++;
+                }
+            }
+            else
+            {
+                // Se abbiamo già trovato un perimetro in precedenza coloro le celle finchè non trovo un bordo esterno
+                if (piano[i][j + 1] != BORDO_ESTERNO) // Non controllo l'indice che uso viste le ipotesi
+                    piano[i][j] = 1;                  // Qui si potrebbe inserire un numero differente per visualizzare dove sono state modificate le cella (vedere la commit af812f9ad90e4bf274d3bda312b9f65902f6e398 era stato fatto)
+                else
+                    interno = false;
+            }
+        }
+    }
+
+    // Sistemo i dati cambiando i BORDO_ESTERNO con 0
+    for (i = 0; i < A; i++)
+        for (j = 0; j < L; j++)
+            if (piano[i][j] == BORDO_ESTERNO)
+                piano[i][j] = 0;
 }
