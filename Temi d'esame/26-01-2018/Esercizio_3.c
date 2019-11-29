7565cecb76b3010079729b2d52c80414addf4f4e
Paolo <pertino.liceograssi@gmail.com>
2019-11-29 08:33:19 +0100

    completato esercizio 3

--- Temi d'esame/26-01-2018/Esercizio_3.c
+++ Temi d'esame/26-01-2018/Esercizio_3.c
@@ -1,5 +1,17 @@
+/*
+  Autore : Paolo Pertino
+  Esercizio 3 - Tema Esame 26-01-2018
+  Descrizione : data una lista dinamica in ingresso, la funzione cancellaElementoMedio elimina l'elemento situato al
+                centro della lista. (dove con posizione centrale si intende (NUMERO_ELEMENTI + 1)/2 ). Se la lista è
+                vuota non verrà ovviamente cancellato nulla.
+
+  Tempo impiegato : 17 minuti __ scrittura funzione cancellaElementoMedio
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+
+#define TERMINATORE '/'
 
 typedef struct ELEMENTO
 {
@@ -9,11 +21,28 @@ typedef struct ELEMENTO
 typedef Carattere *Lista;
 
 void cancellaElementoMedio(Lista *list);
-void leggiLista(Lista toRead);
+void mostraLista(Lista toRead);
 void aggiungiElemento(Lista *toCompile,char Elemento);
 
 int main(int argc, char const *argv[]) {
-  /* code */
+  Lista lista;
+  char toAdd;
+
+  lista = NULL;
+
+  do
+  {
+    printf("Inserisci un carattere : (Carattere di STOP : /)");
+    scanf("%c", &toAdd);
+    fflush(stdin);
+    if (toAdd != TERMINATORE)
+      aggiungiElemento(&lista, toAdd);
+  } while(toAdd != TERMINATORE);
+
+  mostraLista(lista);
+  cancellaElementoMedio(&lista);
+  mostraLista(lista);
+
   return 0;
 }
 
@@ -27,24 +56,24 @@ void cancellaElementoMedio(Lista *list)
 
   ppElemento = list;
   puntNext = *list;
-  puntCor = *lits;
+  puntCor = *list;
   saltaGiro = 1;
 
-  while(puntNext->prox != NULL)
+  while(puntNext->pProx != NULL)
   {
     if(saltaGiro == 1)
     {
-      puntNext = puntNext->prox;
+      puntNext = puntNext->pProx;
       saltaGiro = -1;
     } else {
-      puntNext = puntNext->prox;
-      ppElemento = &(puntCor->prox);
-      puntCor = puntCor->prox;
+      puntNext = puntNext->pProx;
+      ppElemento = &(puntCor->pProx);
+      puntCor = puntCor->pProx;
       saltaGiro = 1;
     }
   }
 
-  temp = puntCor->prox;
+  temp = puntCor->pProx;
   free(puntCor);
   *ppElemento = temp;
 }
@@ -54,13 +83,28 @@ void aggiungiElemento(Lista *toCompile, char Elemento)
   Lista punt;
   if (*toCompile == NULL)
   {
-    punt = malloc(sizeof(numeroLista));
-    punt->numero = Elemento;
-    punt->prox = NULL;
+    punt = malloc(sizeof(Carattere));
+    punt->Dato = Elemento;
+    punt->pProx = NULL;
     *toCompile = punt;
   }
   else
   {
-    aggiungiElemento(&((*toCompile)->prox), Elemento);
+    aggiungiElemento(&((*toCompile)->pProx), Elemento);
+  }
+}
+
+void mostraLista(Lista toRead)
+{
+  int contatore;
+
+  if(toRead==NULL) { printf("\nLista vuota."); return; }
+
+  contatore = 1;
+  while (toRead != NULL)
+  {
+    printf("\nElemento %d : %c", contatore, toRead->Dato);
+    toRead = toRead->pProx;
+    ++contatore;
   }
 }
