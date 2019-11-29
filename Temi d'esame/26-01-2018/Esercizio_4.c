1a1fb9ce66d3c11e07c4d8011c0dcd8281244872
Paolo <pertino.liceograssi@gmail.com>
2019-11-29 12:16:08 +0100

    Completato esercizio 4

--- Temi d'esame/26-01-2018/Esercizio_4.c
+++ Temi d'esame/26-01-2018/Esercizio_4.c
@@ -1,7 +1,9 @@
 /*
   Autore : Paolo Pertino
   Esercizio 4 - Tema Esame 26-01-2018
-  Descrizione :
+  Descrizione : data una lista dinamica classica di interi, la funzione CostruisciBidir crea una nuova lista bidirezionale
+                , ovvero una lista dinamica che olre ad avere il puntatore all'elemento succesivo, ne hanno uno che
+                punta all'elemento precedente.
 
   Tempo impiegato : ?? minuti __ scrittura funzione CostruisciBidir
 */
@@ -24,9 +26,29 @@ typedef struct LISTABIDIM
 typedef ElemListaBiDim *ListaBidirezionale;
 
 ListaBidirezionale CostruisciBidir(ListaMonoDirezionale L);
+void aggiungiElemento(ListaMonoDirezionale *toCompile, int Elemento);
+void mostraLista(ListaMonoDirezionale toRead);
 
 int main(int argc, char const *argv[]) {
+  ListaMonoDirezionale listaIniziale;
+  ListaBidirezionale listaFinale;
+  int toAdd;
 
+  listaIniziale = NULL;
+  listaFinale = NULL;
+
+  do
+  {
+    printf("LISTA 1 - Inserisci un numero : (STOP quando numero < 0) ");
+    scanf("%d", &toAdd);
+    if (toAdd >= 0)
+      aggiungiElemento(&listaIniziale, toAdd);
+  } while (toAdd >= 0);
+
+  mostraLista(listaIniziale);
+  listaFinale = CostruisciBidir(listaIniziale);
+
+  printf("\nLista Costruita...");
   return 0;
 }
 
@@ -43,4 +65,33 @@ ListaBidirezionale CostruisciBidir(ListaMonoDirezionale L)
   if(l1!=NULL) l1->pPrec = l2;
   l2->pPrec = NULL;
   return l2;
+}
+
+void aggiungiElemento(ListaMonoDirezionale *toCompile, int Elemento)
+{
+  ListaMonoDirezionale punt;
+  if (*toCompile == NULL)
+  {
+    punt = malloc(sizeof(ElemListaMonoDim));
+    punt->info = Elemento;
+    punt->pNext = NULL;
+    *toCompile = punt;
+  }
+  else
+  {
+    aggiungiElemento(&((*toCompile)->pNext), Elemento);
+  }
+}
+
+void mostraLista(ListaMonoDirezionale toRead)
+{
+  int contatore;
+
+  contatore = 1;
+  while (toRead != NULL)
+  {
+    printf("\nElemento %d : %d", contatore, toRead->info);
+    toRead = toRead->pNext;
+    ++contatore;
+  }
 }
