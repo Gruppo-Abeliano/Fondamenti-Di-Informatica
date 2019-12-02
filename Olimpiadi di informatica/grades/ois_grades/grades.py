fd0f6f72bf6d5f462545b62ed9eb603a9482fd9d
NidasioAlberto <nidasioalberto@gmail.com>
2019-12-02 16:54:11 +0100

    Completato quiz grades

---
+++ Olimpiadi di informatica/grades/ois_grades/grades.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# NOTE: it is recommended to use this even if you don't understand the following code.
+
+
+# Read input data
+N = int(input().strip())
+G = list(map(int, input().strip().split()))
+
+
+# Insert your code here
+
+
+# Print the result
+print(42)
