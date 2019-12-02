71f6048d5b96e915e6a6603cffbe71c0e08f7e2f
NidasioAlberto <nidasioalberto@gmail.com>
2019-12-02 16:53:43 +0100

    Completato quiz smartphone

---
+++ Olimpiadi di informatica/smartphone/ois_smartphone/smartphone.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# NOTE: it is recommended to use this even if you don't understand the following code.
+
+
+N = int(input().strip())
+P = list(map(int, input().strip().split()))
+
+
+# insert your code here
+
+
+print(42)  # print the result
